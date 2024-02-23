27a9f1d7aa6ae0740f50a0ad94770d34f907a290
Shreenath Omar <ASUS@shreenath.com>
2024-02-24 00:34:41 +0530

    latest changes

---
+++ automationspeed2value/src/main/java/org/operators/com/UnaryOperators01.java
@@ -0,0 +1,18 @@
+package org.operators.com;
+
+public class UnaryOperators01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=10;
+		System.out.println(i++);
+		System.out.println(i);
+		System.out.println(i--);
+		System.out.println(++i);
+		System.out.println(--i);
+		System.out.println(i++);
+
+	}
+
+}
---
+++ automationspeed2value/src/main/java/org/operators/com/UnaryOperators02.java
@@ -0,0 +1,17 @@
+package org.operators.com;
+
+public class UnaryOperators02 {
+	
+	public static void test(int i)
+	{
+		System.out.println(i);
+	}
+	
+	
+	public static void main(String[] args) {
+		int i=0;
+		test(i++);
+		System.out.println(i);
+	}
+
+}
