cd68f644152fb7ec0ef60cfe3b132c69044a953d
Shreenath Omar <ASUS@shreenath.com>
2024-03-06 01:22:59 +0530

    hiii

---
+++ automationspeed2value/src/main/java/org/datatypes/com/DataTypes06.java
@@ -0,0 +1,28 @@
+package org.datatypes.com;
+
+public class DataTypes06 {
+
+	public static void main(String[] args) {
+		//variable declaration
+		//variable intialization
+		byte a=100;
+		char b='A';
+		boolean c=true;
+		short d=200;
+		int e=300;
+		long f=400;
+		float g=10.6f;
+		double h=11.23;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println("This is fourth datatype class");
+
+	}
+
+}
