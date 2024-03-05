f9949d0cfcadf2d571936e3bdb66c1eff34f5d7e
Shreenath Omar <ASUS@shreenath.com>
2024-03-05 19:36:48 +0530

    new changes

---
+++ automationspeed2value/src/main/java/org/dateclass/com/DateFormat01.java
@@ -0,0 +1,15 @@
+package org.dateclass.com;
+
+import java.util.Date;
+
+public class DateFormat01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Date date=new Date();
+		System.out.println(date);
+
+	}
+
+}
--- automationspeed2value/src/main/java/org/wrapperclass/com/WrapperClassAutoUnBoxing0.java
+++ automationspeed2value/src/main/java/org/wrapperclass/com/WrapperClassAutoUnBoxing0.java
@@ -1,12 +1,12 @@
 package org.wrapperclass.com;
 
-public class WrapperClassAutoUnBoxing01 {
+public class WrapperClassAutoUnBoxing0 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int i=200;
-		Integer j=i;
+		Integer i=300;
+		int j=i;
 		System.out.println(j);
 		
 
